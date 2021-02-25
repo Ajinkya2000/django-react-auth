43e0b4e3122a4fca40f3efb5f64bd680f66649d4
Ajinkya <ajinkya.deshpande0000@gmail.com>
2021-02-25 17:21:54 +0530

    cleaned some frontend boilerplate

--- backend/backend_auth/auth_app/serializers.py
+++ backend/backend_auth/auth_app/serializers.py
@@ -29,7 +29,7 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     email = serializers.CharField(max_length=300, required=True)
-    password = serializers.CharField(required=True, write_only=True)
+    # password = serializers.CharField(required=True, write_only=True)
 
     name = serializers.SerializerMethodField(read_only=True)
     isAdmin = serializers.SerializerMethodField(read_only=True)
