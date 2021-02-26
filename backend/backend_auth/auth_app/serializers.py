be35ad3319f74cd42b9f52e2fbc31b19d3b6af6b
kashishagrawal1907 <kashish.agrawal1907@gmail.com>
2021-02-26 06:08:26 +0530

    updated signup api

--- backend/backend_auth/auth_app/serializers.py
+++ backend/backend_auth/auth_app/serializers.py
@@ -17,7 +17,6 @@ class LoginSerializer(TokenObtainPairSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
 
     class Meta:
         model = User
@@ -30,25 +29,11 @@ class RegisterSerializer(UserSerializer):
     first_name = serializers.CharField(required=True)
     last_name = serializers.CharField(required=True)
     email = serializers.EmailField(required=True)
-=======
-    email = serializers.CharField(max_length=300, required=True)
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
-
-
-class UserSerializerWithToken(UserSerializer):
->>>>>>> f546c0c468d42759e53cca3d9390a79c92af98ee
     token = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = User
-<<<<<<< HEAD
         fields = ('id', 'first_name', 'username', 'last_name', 'email', 'password', 'token')
-=======
-        fields = ('id', 'username', 'last_name', 'first_name', 'email', 'token')
->>>>>>> f546c0c468d42759e53cca3d9390a79c92af98ee
 
     def get_token(self, obj):
         token = RefreshToken.for_user(obj)
